8bc3a5bac1b42e962d9572359bf67aafd70d1023
Ryan Frisch <nathanielrfrisch@gmail.com>
2020-12-06 17:26:02 -0700

    added models and applicantDash template

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, aboutPageView, applicantDashPageView
 
 urlpatterns = [
     path("", indexPageView, name="index"),
+    path("about/", aboutPageView, name="about"),
+    path("applicantdash/", applicantDashPageView, name="applicantdash"),
+    
     #  path("<int:{{ user_id }}>/", userDashPageView, name="use dash"),  MAYBE DO SOMETHING LIKE THIS FOR USER LOGIN
                                             # Just need a way to specify the user_id that would be there
 ]
--- search/views.py
+++ search/views.py
@@ -5,6 +5,18 @@ from django.http import HttpResponse
 def indexPageView(request) :
     return render(request, 'search/searchIndex.html')
 
+def aboutPageView(request) :
+    return render(request, 'search/about.html')
+
+def applicantDashPageView(request) :
+    data = 'test' #get relevant user object based on ID stored somewhere?
+
+    context = {
+        "user" : data
+    }
+
+    return render(request, 'search/applicantDash.html', context)
+
 # perhaps instead of typing user in url you can just 
 # def userDashPageView(request, person.person_id) :
 #     return HttpResponse(person_id)
